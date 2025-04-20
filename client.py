5bff85737f40c326a33073ebf6764030591739a2
Biazoli <barrosbleo@gmail.com>
2025-04-20 13:03:24 -0300

    Add arguments to send custom message

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
+import sys
 from websockets.sync.client import connect
 
-def hello():
+def hello(message):
     with connect("ws://localhost:8765") as websocket:
-        websocket.send("Hello, world!")
-        print(f"Sent message: Hello, world!")
+        websocket.send(message)
+        print(f"Sent message: {message}")
         response = websocket.recv()
         print(f"Received response: {response}")
 
-hello()
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        message = sys.argv[1]
+    else:
+        message = "Hello, world!"
+    hello(message)
